0a22ca241e78af193cbe42eae9f5011417c13aab
Abhishek Gupta <abhishekgupta2248@gmail.com>
2022-10-05 07:11:14 +0530

    completing the register UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import {Button} from 'antd';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Doctro</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path = '/login' element = {<Login/>}/>
+        <Route path = '/register' element = {<Register/>}/>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/Register.js
@@ -0,0 +1,37 @@
+import { Button, Form, Input } from 'antd'
+import React from 'react'
+
+function Register() {
+  return (
+    <div className='authentication'>
+      <div className='authentication-form card p-4'>
+        <h1 className='card-title'>Sign Up</h1>
+        <Form layout= 'vertical'>
+
+          <Form.Item label= 'Name' name= 'name'>
+            <Input placeholder= 'Name' />
+          </Form.Item>  
+
+          <Form.Item label= 'Email' name= 'email'>
+            <Input placeholder= 'Email' />
+          </Form.Item>  
+
+          <Form.Item label= 'Password' name= 'password'>
+            <Input placeholder= 'Password' />
+          </Form.Item>  
+
+          <Button className='primary-button mt-3'>
+            Sign Up
+          </Button>
+
+          
+
+        </Form>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Register
