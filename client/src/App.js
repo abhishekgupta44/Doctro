a4be36025d830f90e727eb9cd4606f0bbc95856d
Abhishek Gupta <abhishekgupta2248@gmail.com>
2022-10-05 15:07:24 +0530

    completing the user registration endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,15 @@ import {Button} from 'antd';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import {Toaster} from 'react-hot-toast';
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position= 'top-center' reverseOrder= {false}/> 
       <Routes>
-
         <Route path = '/login' element = {<Login/>}/>
         <Route path = '/register' element = {<Register/>}/>
-
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,13 +1,26 @@
-import { Button, Form, Input } from 'antd'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button, Form, Input } from 'antd';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 function Register() {
 
-  const onFinish = (values) => {
-    console.log('Received values of the form', values);
-
-  }
+  const navigate = useNavigate();
+  const onFinish = async(values) => {
+    try {
+      const response = await axios.post('/api/user/register', values);
+      if(response.data.success) {
+        toast.success(response.data.message);
+        toast("Redirecting to Login page");
+        navigate("/login");
+      }else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error('Something went wrong');
+    }
+  };
 
   return (
     <div className='authentication'>
@@ -36,7 +49,6 @@ function Register() {
           </Link>
 
           
-
         </Form>
 
       </div>
