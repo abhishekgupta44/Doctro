089f945189bf18e879851dfb29681af4efa2fe2a
Abhishek Gupta <abhishekgupta2248@gmail.com>
2022-10-07 11:43:50 +0530

    completing the layout structure for all users admin, user, doctor

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,30 +1,33 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import axios from "axios";
+import Layout from "../components/Layout";
 
 function Home() {
-
-  const getData = async() => {
-
+  const getData = async () => {
     try {
-      const response = await axios.post('/api/user/get-user-info-by-id', {},
-      {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("token"),
-        },
-      });
+      const response = await axios.post(
+        "/api/user/get-user-info-by-id",
+        {},
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
       console.log(response.data);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-
-    getData()
-  }, [])
+    getData();
+  }, []);
   return (
-    <div>Home</div>
-  )
+    <Layout>
+      <h1>Home page</h1>
+    </Layout>
+  );
 }
 
-export default Home
+export default Home;
